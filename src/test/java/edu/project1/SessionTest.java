f6a30f7dd603d6078054c7965bee7d18df103c00
Tagir <gtagir11@yandex.ru>
2023-10-22 23:56:12 +0300

    fixcheckstyle

--- src/test/java/edu/project1/SessionTest.java
+++ src/test/java/edu/project1/SessionTest.java
@@ -1,9 +1,8 @@
 package edu.project1;
 
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
 
 public class SessionTest {
     @Test
